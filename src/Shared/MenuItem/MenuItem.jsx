2f2ef7754f5b0ea6753c542f560e5f2a0415467f
Muhammad Tamim <mdt40234@gmail.com>
2024-09-19 04:34:24 +0600

    Menu item  and popular added

---
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MenuItem = ({ name, recipe, image, price }) => {
+    return (
+        <div className='flex gap-3 my-4'>
+            <img style={{ borderRadius: '0 300px 300px 300px' }} className='w-[100px]' src={image} alt="" />
+            <div className="description">
+                <h2>{name}</h2>
+                <p>{recipe}</p>
+            </div>
+            <p className='text-orange-600'>${price}</p>
+        </div>
+    );
+};
+
+export default MenuItem;
--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../component/SectionTitle/SectionTitle';
+import MenuItem from '../../../Shared/MenuItem/MenuItem';
+
 
 const PopularMenu = () => {
+    const [menu, Setmenu] = useState([]);
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                const PopularItem = data.filter(item => item.category === "popular")
+                Setmenu(PopularItem);
+                // console.log(PopularItem);
+            });
+
+    }, [])
     return (
         <div>
             <SectionTitle
                 SubHeading={"Check it out"}
                 Heading={"FROM OUR MENU"}
             ></SectionTitle>
+            <div className='md:mx-20 grid grid-cols-2'>
+                {
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        name={item.name}
+                        recipe={item.recipe}
+                        image={item.image}
+                        price={item.price}
+                    ></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
