9db7b1c3e48d74ca204bf4febbd98cb183dc4b52
Muhammad Tamim <mdt40234@gmail.com>
2024-09-20 07:59:36 +0600

    added parallax on featured backgroung image useing bg opacity & display fixed

--- src/component/SectionTitle/SectionTitle.jsx
+++ src/component/SectionTitle/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SectionTitle = ({ SubHeading, Heading, HeadingColor }) => {
     return (
-        <div className='md:size-3/12 my-24 font-bold text-center mx-auto'>
+        <div className='md:size-4/12 my-24 font-bold text-center mx-auto'>
             <p className='text-yellow-500 md:text-xl'>--- {SubHeading} ---</p>
             <h2 className={`md:text-4xl	mt-2 border-y-4 py-4 ${HeadingColor}`}>{Heading}</h2>
         </div>
--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,27 +5,29 @@ import FeaturedImg from '../../../assets/assets/home/featured.jpg'
 
 const Featured = () => {
     return (
-        <div className='bg-cover bg-center text-white my-10 pt-1 bg-black bg-opacity-20'
+        <div className='bg-cover bg-center bg-fixed '
             style={{ backgroundImage: `url(${FeaturedImg})` }}
         >
-            <div>
-                <SectionTitle
-                    SubHeading={'Check it out'}
-                    Heading={'FROM OUR MENU'}
-                    HeadingColor={'text-white'}
-                ></SectionTitle>
-            </div>
-            <div className={`md:flex justify-center items-center px-24 gap-10 pb-16`}>
+            <div className='bg-opacity-50 bg-slate-900 text-white   my-10 pt-1'>
                 <div>
-                    <img src={FeaturedImg} alt="" />
+                    <SectionTitle
+                        SubHeading={'Check it out'}
+                        Heading={'FROM OUR MENU'}
+                        HeadingColor={'text-white'}
+                    ></SectionTitle>
                 </div>
-                <div>
-                    <h3 className='text-2xl'>March 20, 2023</h3>
-                    <h3 className='text-2xl'>WHERE CAN I GET SOME?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere,
-                        deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium
-                        tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
-                    <button className="mt-3 btn btn-outline border-b-4 border-black">READ MORE</button>
+                <div className={`md:flex justify-center items-center px-24 gap-10 pb-16 `}>
+                    <div>
+                        <img src={FeaturedImg} alt="" />
+                    </div>
+                    <div>
+                        <h3 className='text-2xl'>March 20, 2023</h3>
+                        <h3 className='text-2xl'>WHERE CAN I GET SOME?</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptate facere,
+                            deserunt dolores maiores quod nobis quas quasi. Eaque repellat recusandae ad laudantium
+                            tempore consequatur consequuntur omnis ullam maxime tenetur.</p>
+                        <button className="mt-3 btn btn-outline border-b-4 border-black">READ MORE</button>
+                    </div>
                 </div>
             </div>
         </div>
