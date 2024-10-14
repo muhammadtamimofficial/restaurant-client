9b405a264e87c1892e5d300c62f5094b4750b95d
Muhammad Tamim <mdt40234@gmail.com>
2024-10-15 04:52:57 +0600

    our menu page today's offers ,DESSERTS & pizza added

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../../Shared/Cover/Cover';
 import img from '../../../../src/assets/assets/menu/banner3.jpg'
+import MenuCategory from '../MenuCategory/MenuCategory';
+import useMenu from '../../../hooks/useMenu';
+import dessertImg from '../../../assets/assets/menu/dessert-bg.jpeg'
+// import dessertImg from '../../../assets/assets/menu/dessert-bg.jpeg'
+// import dessertImg from '../../../assets/assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../assets/assets/menu/pizza-bg.jpg'
+// import dessertImg from '../../../assets/assets/menu/dessert-bg.jpeg'
 
 const Menu = () => {
+    const [menu] = useMenu();
+    const offered = menu.filter(item => item.category === "offered")
+    const pizza = menu.filter(item => item.category === "pizza")
+    console.log(offered);
+
     return (
         <div>
             <Helmet>
@@ -18,7 +30,26 @@ const Menu = () => {
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste repudiandae a nihil cupiditate voluptatibus
                         ducimus illum quidem voluptate magnam officia!`}
             ></Cover>
-
+            <MenuCategory
+                // img={img}
+                subHeading={"Don't miss"}
+                heading={"TODAY'S OFFER"}
+                category={offered}
+            ></MenuCategory>
+            <MenuCategory
+                img={dessertImg}
+                title={"DESSERTS"}
+                subTitle={`Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}
+                category={offered}
+            ></MenuCategory>
+            <MenuCategory
+                category={pizza}
+                img={pizzaImg}
+                title={"Pizza"}
+                subTitle={`Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}
+            ></MenuCategory>
         </div >
     );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import SectionTitle from '../../../component/SectionTitle/SectionTitle';
+import MenuItem from '../../../Shared/MenuItem/MenuItem';
 
-const MenuCategory = () => {
+const MenuCategory = ({ subHeading, heading, title, subTitle, img, category }) => {
     return (
         <div>
-            <h1 className='text-center'>MenuCategory</h1>
+            {
+                heading && <SectionTitle
+                    SubHeading={subHeading}
+                    Heading={heading}
+                ></SectionTitle>
+            }
+            {img &&
+                <div className="h-[500px] bg-cover bg-center text-white text-center flex items-center justify-center"
+                    style={{
+                        backgroundImage: `url(${img})`
+                    }}>
+                    <div className="px-20 py-16 bg-slate-800 w-2/3 opacity-70">
+                        <h1 className='text-3xl font-bold'>{title}</h1>
+                        <p>{subTitle}</p>
+                    </div>
+                </div>}
+            <div className='md:mx-20 grid grid-cols-2 my-20'>
+                {
+                    category.map(item => <MenuItem
+                        key={item._id}
+                        name={item.name}
+                        recipe={item.recipe}
+                        image={item.image}
+                        price={item.price}
+                    ></MenuItem>)
+                }
+            </div>
         </div>
     );
 };
