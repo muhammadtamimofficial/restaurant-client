6ee69bb0a014bc1a23e8f285e83b67f95ee57ea8
Muhammad Tamim <mdt40234@gmail.com>
2024-10-15 05:27:04 +0600

    cover edited

--- src/Shared/Cover/Cover.jsx
+++ src/Shared/Cover/Cover.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Cover = ({ img, title, article }) => {
     return (
         <div
-            className="hero h-[400px] uppercase bg-cover bg-no-repeat"
+            className="hero h-[600px] text-white"
             style={{
                 backgroundImage: `url(${img})`,
             }}>
-            <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-neutral-content text-center">
-                <div className="max-w-md">
+            <div className="hero-overlay"></div>
+            <div className=" flex text-center bg-slate-800 w-2/3 text-center bg-opacity-60">
+                <div className="p-20">
                     <h1 className="mb-5 text-5xl font-bold">{title}</h1>
                     <p className="mb-5">
                         {article}
