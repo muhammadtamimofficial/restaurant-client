60bd78eddacd2569088a890c8e668809e77b901d
Muhammad Tamim <mdt40234@gmail.com>
2024-10-15 05:11:45 +0600

    get some ceraction on our menu page

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -61,7 +61,7 @@ const Menu = () => {
             <MenuCategory
                 category={soup}
                 img={soupImg}
-                title={"Salad"}
+                title={"Soup"}
                 subTitle={`Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, 
                     when an unknown printer took a galley of type and scrambled it to make a type specimen book.`}
             ></MenuCategory>
