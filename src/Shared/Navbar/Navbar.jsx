4c3a95445b61dbfc416da733cb6fb6ca5cc355f8
muhammadtamimworkplace <muhammad.tamim.workplace@gmail.com>
2024-12-06 02:21:37 +0600

    Revert "display loged user info on navbar"

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,25 +1,12 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Providers/AuthProvider';
 
 const Navbar = () => {
-    const { user, logOut } = useContext(AuthContext)
-
+    const { logOut } = useContext(AuthContext);
     const handleLogOut = () => {
         logOut()
     }
-    const menubar = <>
-        <li><Link to={'/'}>Home</Link></li>
-        <li><Link to={'menu'}>Our Menu</Link></li>
-        <li><Link to={'order/Salad'}>Order Food</Link></li>
-        {
-            user ? <button><button className='ml-2'>{user.displayName}</button> <button className='ml-4' onClick={handleLogOut}>Logout</button></button>
-                :
-                <button><Link to={'/login'}>Login</Link></button>
-        }
-    </>
-    // console.log();
-
     return (
         <>
             <div class="navbar fixed z-10 opacity-40 bg-black font-bold text-white py-3 px-16 ">
@@ -42,28 +29,22 @@ const Navbar = () => {
                         <ul
                             tabindex="0"
                             class="menu menu-sm text-blue-900 dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            {/* <li><Link to={'/'}>Home</Link></li>
+                            <li><Link to={'/'}>Home</Link></li>
                             <li><Link to={'menu'}>Our Menu</Link></li>
-                            <li><Link to={'order/Salad'}>Order Food</Link></li> */
-                                menubar
-                            }
+                            <li><Link to={'order/Salad'}>Order Food</Link></li>
                         </ul>
                     </div>
                     <Link to={'/'} class="btn btn-ghost text-xl">daisyUI</Link>
                 </div>
                 <div class="navbar-center hidden lg:flex">
                     <ul class="menu menu-horizontal px-1 ">
-                        {menubar}
+                        <li><Link to={'/'}>Home</Link></li>
+                        <li><Link to={'menu'}>Our Menu</Link></li>
+                        <li><Link to={'order/Salad'}>Order Food</Link></li>
                     </ul>
                 </div>
                 <div class="navbar-end">
-                    {/* <a class="btn" onClick={handleLogOut}>logOut</a> */}
-                    <div className="displayUser bg-gray-950">
-                        <img src={''}
-                            title='Not clickable, only for view'
-                            alt="" width='48px' height={'48px'}
-                            className='rounded-full opacity-100' />
-                    </div>
+                    <a class="btn" onClick={handleLogOut}>logOut</a>
                 </div>
             </div >
         </>
